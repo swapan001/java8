36c4bfc59887bd5c8da3f03eeca0377f4258411f
swapan001 <swan03228@gmail.com>
2026-01-01 01:54:44 +0530

    functional interface with lambda in java-8

--- src/main/java_8/funcinterface/Example.java
+++ src/main/java_8/funcinterface/Example.java
@@ -6,11 +6,37 @@ package main.java_8.funcinterface;
 interface Greeting {
     void sayHello(String name);
 }
+
+@FunctionalInterface
+interface AddTwoNum{
+    int add(int a, int b);
+}
+
+@FunctionalInterface
+interface DivNum{
+    double div(double a, double b);
+}
+
+@FunctionalInterface
+interface MulNum{
+    double mul(double a, double b);
+}
+
 public class Example {
     public static void main(String[] args) {
 
         /* with  lambda expression */
         Greeting greeting = (name) -> System.out.println("Hello, " + name + "!");
         greeting.sayHello("swapan001");
+
+        AddTwoNum addTwoNum = (a,b) -> a+b;
+        System.out.println("Result: "+addTwoNum.add(10,20));
+
+        DivNum divNum=(a,b)->(a/b);
+        System.out.println("Result: "+divNum.div(10,4));
+
+        MulNum mulNum=(a, b)->(a*b);
+        System.out.println("Result: "+mulNum.mul(10, 4));
+
     }
 }
