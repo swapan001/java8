6e499d64ee0189879b3bf4e2cacd55ba6a64d26f
swapan001 <swan03228@gmail.com>
2026-01-01 23:30:22 +0530

    Using stream filter and map square all evenNumber

--- src/main/java_8/stream_api/Stream.java
+++ src/main/java_8/stream_api/Stream.java
@@ -16,15 +16,27 @@ import java.util.List;
 class StreamFilter{
 
     /* find the employee whose name start with A */
+
+    /* using filter and forEach */
     public static void filterNameStartWith(){
         List<String>nameList= Arrays.asList("Ayan","Shreya","Sayan","Tony","Sumit","Arka","Akshay");
 
         nameList.stream().filter((s)->s.startsWith("A")).forEach(System.out::println);
     }
+
+    public static void squareEvenNumber(){
+        List<Integer>numbers=Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        numbers.stream().filter(n-> (n%2==0)).map(n -> n*n).forEach(System.out::println);
+    }
+
+
+
+
 }
 public class Stream {
     public static void main(String[] args) {
         StreamFilter.filterNameStartWith();
-
+        StreamFilter.squareEvenNumber();
     }
 }
